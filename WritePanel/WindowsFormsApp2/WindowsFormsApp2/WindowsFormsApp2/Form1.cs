4fc05abe25909e0e0c28c6ad556c6157f7e797bf
MasterkitGamer <95971734+MasterkitGamer@users.noreply.github.com>
2023-11-09 16:56:19 +0300

    Add files via upload

---
+++ WritePanel/WindowsFormsApp2/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WritePanel
+{
+    public partial class Form1 : Form
+    {
+        string g;
+        int d, e, f, max;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ABC(textBox1.Text, textBox2.Text, textBox3.Text);
+            Form2 frm2 = new Form2();
+            frm2.otvet = g;
+            frm2.Show();
+        }
+
+        public void ABC(string a, string b, string c)
+        {
+            d = Convert.ToInt32(a);
+            e = Convert.ToInt32(b); 
+            f = Convert.ToInt32(c);
+           if(d+e<=f) { g = "Такого треугольника не существует"; }
+           else if (f + e <= d) { g = "Такого треугольника не существует"; }
+           else if (d + f <= e) { g = "Такого треугольника не существует"; }
+           else
+            {
+                if (d == e)
+                {
+                    if (f == e)
+                    {
+                        g = "Треугольник равностороний";
+                    }
+                    else
+                    {
+                        g = "Треугольник равнобедренный";
+                    }
+                }
+                else
+                {
+                    if (f == e)
+                    {
+                        if (d == e)
+                        {
+
+                        }
+                        else
+                        {
+                            g = "Треугольник равнобедренный";
+                        }
+                    }
+                    else if (f == d)
+                    {
+                      g = "Треугольник равнобедренный";
+                    }
+                    else { g = "Треугольник разносторонний"; }
+                }
+            }
+        }
+    }
+}
---
+++ WritePanel/WindowsFormsApp2/WindowsFormsApp2/WindowsFormsApp2/Form2.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WritePanel
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(300, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать заного";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(279, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "WritePanel";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
